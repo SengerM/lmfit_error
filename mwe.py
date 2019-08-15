1a7c9f8cbafb2294d913b950f840b8e7703fae97
Matias Senger <m.senger@hotmail.com>
2019-08-15 15:26:49 -0500

    adsaddasdadsa

--- mwe.py
+++ mwe.py
@@ -14,9 +14,11 @@ def model_poisson_forced(x, gain, shift, sigma, mu):
 def model_lin(x, gain):
 	return x*gain
 
-active_area = np.genfromtxt('temp_active_area').transpose()
-x_vals = np.genfromtxt('temp_x').transpose()
-ecdf = np.genfromtxt('temp_ecdf').transpose()
+DIRECTORY = 'fail_data'
+
+active_area = np.genfromtxt(DIRECTORY + '/temp_active_area').transpose()
+x_vals = np.genfromtxt(DIRECTORY + '/temp_x').transpose()
+ecdf = np.genfromtxt(DIRECTORY + '/temp_ecdf').transpose()
 
 model = lmfit.Model(model_poisson_forced) # If "model_poisson_forced" is replaced by "model_lin" it works
 params = model.make_params()
@@ -25,11 +27,20 @@ params['shift'].set(value = 0, max = 1, min = -1)
 params['sigma'].set(value = 300**-.5*4)
 params['mu'].set(value = active_area.ravel().std()*.9, min = 0)
 
-fit_result = model.fit(ecdf, params, x = x_vals) # Comment this line and see the data
+# Coment all this block to easilly see the data ------------------------
+fit_result = model.fit(ecdf, params, x = x_vals)
+
+fig, ax = plt.subplots()
+x_axis = np.linspace(min(x_vals),max(x_vals),300)
+ax.plot(x_axis, model.eval(fit_result.params, x = x_axis), label = 'fit')
+ax.plot(x_vals, ecdf, label = 'data')
+ax.legend()
+# ----------------------------------------------------------------------
 
 fig, ax = plt.subplots()
 x_axis = np.linspace(min(x_vals),max(x_vals),300)
 ax.plot(x_axis, model.eval(params, x = x_axis), label = 'fit')
 ax.plot(x_vals, ecdf, label = 'data')
 ax.legend()
+
 plt.show()
